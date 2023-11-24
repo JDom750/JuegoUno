268f7ecd076422fb0bbe25f74803a960d8fe4a7e
JDom750 <Joaquindominguez750@gmail.com>
2023-11-23 21:04:05 -0300

    avanzando

--- src/Uno/UnoMenu.java
+++ src/Uno/UnoMenu.java
@@ -14,19 +14,35 @@ public class UnoMenu {
     }
 
     public static void MostrarMenu() {
-        JFrame frame = new JFrame("Menú de Jugadores");
+        JFrame frame = new JFrame("Menú UNO");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 200);
+        frame.setSize(400, 300);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JTextField jugador1Field = new JTextField(20);
-        JTextField jugador2Field = new JTextField(20);
-        JTextField jugador3Field = new JTextField(20);
+        JLabel tituloLabel = new JLabel("UNO");
+        tituloLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        tituloLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JButton iniciarJuegoButton = new JButton("Iniciar Juego");
         JButton salirButton = new JButton("Salir");
+
+        iniciarJuegoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int cantidadJugadores = obtenerCantidadJugadores();
+                if (cantidadJugadores > 0) {
+                    String[] jugadores = obtenerNombresJugadores(cantidadJugadores);
+                    if (jugadores != null) {
+                        UnoController controller = new UnoController(jugadores);
+                        UnoView view = new UnoView(controller.getPartida(), controller);
+                        view.mostrarVentana();
+                        frame.dispose();
+                    }
+                }
+            }
+        });
 
         salirButton.addActionListener(new ActionListener() {
             @Override
@@ -35,39 +51,44 @@ public class UnoMenu {
             }
         });
 
-        iniciarJuegoButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String jugador1 = jugador1Field.getText();
-                String jugador2 = jugador2Field.getText();
-                String jugador3 = jugador3Field.getText();
-
-                // Validar que se hayan ingresado nombres de jugadores
-                if (!jugador1.isEmpty() && !jugador2.isEmpty() && !jugador3.isEmpty()) {
-                    // Aquí puedes crear tu controlador y vista, y comenzar el juego
-                    String[] jugadores = {jugador1, jugador2, jugador3};
-                    UnoController controller = new UnoController(jugadores);
-                    UnoView view = new UnoView(controller.getPartida(), controller);
-                    view.mostrarVentana();
-                    frame.dispose();  // Cerrar el menú después de iniciar el juego
-                } else {
-                    JOptionPane.showMessageDialog(frame, "Ingrese nombres para todos los jugadores.", "Error", JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-
-        panel.add(new JLabel("Jugador 1:"));
-        panel.add(jugador1Field);
-        panel.add(new JLabel("Jugador 2:"));
-        panel.add(jugador2Field);
-        panel.add(new JLabel("Jugador 3:"));
-        panel.add(jugador3Field);
-        panel.add(Box.createVerticalStrut(10));  // Separación
+        panel.add(Box.createVerticalStrut(20));  // Separación
+        panel.add(tituloLabel);
+        panel.add(Box.createVerticalStrut(20));  // Separación
         panel.add(iniciarJuegoButton);
-        panel.add(Box.createVerticalStrut(10));  // Separación
+        panel.add(Box.createVerticalStrut(20));  // Separación
         panel.add(salirButton);
 
         frame.add(panel);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         frame.setVisible(true);
+    }
+
+    private static int obtenerCantidadJugadores() {
+        String cantidadJugadoresStr = JOptionPane.showInputDialog("Ingrese la cantidad de jugadores (2-10):");
+        try {
+            int cantidadJugadores = Integer.parseInt(cantidadJugadoresStr);
+            if (cantidadJugadores >= 2 && cantidadJugadores <= 10) {
+                return cantidadJugadores;
+            } else {
+                JOptionPane.showMessageDialog(null, "Ingrese un número de jugadores válido (entre 2 y 10).", "Error", JOptionPane.ERROR_MESSAGE);
+                return -1;
+            }
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null, "Ingrese un número válido para la cantidad de jugadores.", "Error", JOptionPane.ERROR_MESSAGE);
+            return -1;
+        }
+    }
+
+    private static String[] obtenerNombresJugadores(int cantidad) {
+        String[] jugadores = new String[cantidad];
+        for (int i = 0; i < cantidad; i++) {
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre del Jugador " + (i + 1) + ":");
+            if (nombre == null || nombre.trim().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Ingrese un nombre válido para todos los jugadores.", "Error", JOptionPane.ERROR_MESSAGE);
+                return null;
+            }
+            jugadores[i] = nombre;
+        }
+        return jugadores;
     }
 }
